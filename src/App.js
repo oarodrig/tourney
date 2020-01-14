b1cc84522857bf9257ade8a9b11c1df9e4e5680e
Omar Rodriguez <oarodriguez.cs@gmail.com>
2020-01-14 06:08:44 -0800

    Update App for development display of Bracket component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Bracket from './Bracket/Bracket';
 
 function App() {
+  const players = [
+    { name: 'Omar' },
+    { name: 'Henry' },
+    { name: 'Craig' },
+    { name: 'Wes' },
+    { name: 'Brad' },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Bracket players={players}/>
   );
 }
 
